3b5a078b49dd8e2dfb99a25bcb18b8e29c21000b
Julie Carlett <95197835+jucarlett@users.noreply.github.com>
2023-07-28 16:57:58 -0300

    adicionado `.editconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-  return <h1>Fih, eu amo você. Sem você nada disso seria possível...
-    nunca esquece que sou extremamente grata a você s2... te amo</h1>;
+  return (
+    <h1>
+      Fih, eu amo você. Sem você nada disso seria possível... nunca esquece que
+      sou extremamente grata a você s2... te amo
+    </h1>
+  );
 }
 
 export default Home;
